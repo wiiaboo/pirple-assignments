7439003488c72fb0c9be64df2437751fc4da6dde
Ricardo Constantino <wiiaboo@gmail.com>
2018-12-01 21:34:47 +0000

    Add configuration file
    Also add it to gitignore, so the end user can change it.

--- restapi/config.js
+++ restapi/config.js
@@ -4,3 +4,23 @@
  * Assume the end user will change it to their needs,
  * so avoid doing too many changes after adding it.
  */
+
+const configs = {
+    staging: {
+        ports: {
+            http: 3000,
+            https: 3001
+        },
+        name: 'Staging'
+    },
+    production: {
+        ports: {
+            http: 5000,
+            https: 5001
+        },
+        name: 'Production'
+    }
+};
+
+ module.exports = (typeof configs[process.env.NODE_ENV] === 'object') ?
+    configs[process.env.NODE_ENV] : configs.staging;
